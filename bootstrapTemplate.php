09144e8e245506fa124109b6559bb6cdd8bf002a
Gabix <gabrielhopp@gmail.com>
2015-06-24 21:28:53 -0300

    caca

---
+++ bootstrapTemplate.php
@@ -0,0 +1,33 @@
+<?php
+$caca = "La caca";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        
+        <title><?= $caca ?></title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <style>
+            /* css */
+        </style>
+    </head>
+    <body>
+
+<h1><?= $caca ?></h1>
+<div>TODO write content</div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+        /* js */
+    </script>
+    </body>
+</html>
